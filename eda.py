d4eb27fe64b64e8511e974006ca78675317a72fc
Javier Pallarés <100472252@alumnos.uc3m.es>
2024-02-28 15:09:09 +0100

    eda basic

---
+++ eda.py
@@ -0,0 +1,144 @@
+from collections import Counter
+
+import pandas as pd
+import numpy as np
+import seaborn as sns  # visualisation
+import matplotlib.pyplot as plt  # visualisation
+
+sns.set(color_codes=True)
+
+file_path = './wind_ava.csv'
+df = pd.read_csv(file_path)
+
+"""GOOGLE COLAB
+from google.colab import drive
+drive.mount('/content/gdrive', force_remount = True)
+file_path = '/content/gdrive/MyDrive/wind_ava.csv'
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(file_path)
+"""
+
+num_instances, num_features = df.shape
+print("Number of instances: ", num_instances)
+print("Number of features: ", num_features)
+variable_types = df.dtypes
+columns = df.columns
+missing_values = df.isnull().sum()
+print("Tipos de variables:")
+for i in range(len(variable_types)):
+    if variable_types[i] in ['float64', 'int64']:
+        print(columns[i], ": ", variable_types[i], "-> numérico. || Missing values: ", missing_values[i])
+    else:
+        print(columns[i], ": ", variable_types[i], "-> categórico. || Missing values: ", missing_values[i])
+
+
+# Mostrar columnas con valores faltantes
+print("Columnas con valores faltantes:")
+if missing_values.sum() == 0:
+    print("No hay columnas con valores faltantes")
+else:
+    for col in columns:
+        if missing_values[col] > 0:
+            print(col, ": ", missing_values[col])
+
+# Columnas constantes
+constant_columns = df.columns[df.nunique() == 1]
+print("Columnas constantes:")
+if len(constant_columns) == 0:
+    print("No hay columnas constantes")
+else:
+    for col in constant_columns:
+        print(col)
+
+# Filas duplicadas
+duplicated_rows = df.duplicated()
+print("Filas duplicadas: ", duplicated_rows.sum())
+
+# Filas vacías
+empty_rows = df.isnull().all(axis=1)
+print("Filas vacías: ", empty_rows.sum())
+
+# Naturaleza del problema (regresión o clasificación)
+problem_type = "Problema de Regresión" if df['energy'].dtype in ['float64', 'int64'] else "Problema de Clasificación"
+print(f"Naturaleza del problema: {problem_type}")
+
+# Eliminación de variables meteorológicas no correspondientes a la localización 13
+print("Columnas relevantes:")
+relevant_columns = [col for col in df.columns if col.endswith(".13") or col in ['datetime', 'energy']]
+df_relevant = df[relevant_columns]
+
+# Renombrado de columnas
+# Renombrar las columnas relevantes de manera más corta
+df_relevant = df_relevant.rename(columns={
+    't2m.13': 'temp_2m',
+    'u10.13': 'wind_U_10m',
+    'v10.13': 'wind_V_10m',
+    'u100.13': 'wind_U_100m',
+    'v100.13': 'wind_V_100m',
+    'cape.13': 'convective_energy',
+    'flsr.13': 'log_surface_roughness_heat_forecast',
+    'fsr.13': 'surface_roughness_forecast',
+    'iews.13': 'eastward_turbulent_stress',
+    'inss.13': 'northward_turbulent_stress',
+    'lai_hv.13': 'lai_high_vegetation',
+    'lai_lv.13': 'lai_low_vegetation',
+    'u10n.13': 'neutral_wind_10m_U',
+    'v10n.13': 'neutral_wind_10m_V',
+    'stl1.13': 'soil_temp_level_1',
+    'stl2.13': 'soil_temp_level_2',
+    'stl3.13': 'soil_temp_level_3',
+    'stl4.13': 'soil_temp_level_4',
+    'sp.13': 'surface_pressure',
+    'p54.162.13': 'vertical_integral_temp',
+    'p59.162.13': 'vertical_integral_div_kinetic_energy',
+    'p55.162.13': 'vertical_integral_water_vapour',
+    'datetime': 'datetime',
+    'energy': 'energy'
+})
+
+print(df_relevant.head(6))
+
+# TODO ¿¿Outliers??
+
+
+
+
+
+
+# Detectar outliers
+def detect_outliers(df, n, features):
+    outlier_indices = []
+    for col in features:
+        if df[col].dtype not in [np.float64, np.int64]:
+            continue  # Skip datetime or non-numeric columns
+        Q1 = np.percentile(df[col], 25)
+        Q3 = np.percentile(df[col], 75)
+        IQR = Q3 - Q1
+        outlier_step = 1.5 * IQR
+        outlier_list_col = df[(df[col] < Q1 - outlier_step) | (df[col] > Q3 + outlier_step)].index
+        outlier_indices.extend(outlier_list_col)
+    outlier_indices = Counter(outlier_indices)
+    multiple_outliers = list(k for k, v in outlier_indices.items() if v > n)
+    return multiple_outliers
+
+
+outliers = detect_outliers(df_relevant, 2, df_relevant.columns)
+print("Número de outliers: ", len(outliers))
+
+# Visualización de outliers
+def plot_outliers(df, features):
+    for col in features:
+        plt.figure()
+        sns.boxplot(x=df[col])
+        plt.title(col)
+        plt.show()
+
+plot_outliers(df_relevant, df_relevant.columns)
+
+# Eliminación de outliers
+def remove_outliers(df, outliers):
+    df = df.drop(outliers, axis=0).reset_index(drop=True)
+    return df
+
+df_relevant = remove_outliers(df_relevant, outliers)
